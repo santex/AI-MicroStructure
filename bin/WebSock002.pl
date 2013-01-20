b54d588f21db0ccc21b271030fc336b270d50d5f
hagen <hagen@sante12.(none)>
2013-01-20 15:34:38 +0100

    ws

--- bin/WebSock002.pl
+++ bin/WebSock002.pl
@@ -15,20 +15,7 @@ my $DEBUG_TO_SYSLOG=1;
 my $LOGGER_EXE="/usr/bin/logger";
 my $PORT = 3000;
 my $JSON = {};
-my $pwd = $PWD;#"/var/www/vhosts";
-
-
-
-sub Dbg {
-  my $level=shift @_;
-  my $msg = shift @_;
-  # If the $DEBUG level exceeds the level at which we log this mess
-      my @args=`echo '$0 $msg' | $LOGGER_EXE`;
-
-  return sprintf("\n",$msg);
-}
-
-
+my $pwd = $PWD;
 
 sub printer {
   my ($msg) = @_;
@@ -37,7 +24,6 @@ sub printer {
      $msg = "space" unless($msg);
 
      $cmd->{action} = sprintf(`perl $pwd/nav.pl $msg | data-freq --limit 100 | egrep "[a-z|A-Z]" | grep -v "#"`);
-     #sprintf(`perl /home/hagen/myperl/AI-MicroStructure-0.01/teswikilist05.pl $msg | data-freq --limit 100 | egrep ": [a-zA-Z]" | grep -v "#"`);
 
      $cmd->{action} = [map{my @a= [split(":",$_)]; $a[0][0]=~ s/ //g; $_={neighbour => $a[0][1], spawn => $a[0][0]};}split("\n",$cmd->{action})];
 
@@ -47,70 +33,8 @@ sub printer {
                                                       "callback" => "makeList",
                                                       "responce" => $cmd->{action}});
 
-
-
-#      print Dumper [$msg,$cmd];
-
       return $cmd->{json};
 
-}
-sub printerOverw {
-  my ($self,$msg) = @_;
-  my $cmd = {};
-
-     $msg = "space" unless($msg);
-    
-    my $cmd = `perl $pwd/test005.pl $msg`;
-    
-     my @common = decode_json($cmd) unless($cmd!~/{/);
-      
-      
-    foreach(@common){
-      my $entry = $_->[0];
-      foreach(keys %$entry){
-   #   $self->send_frame( JSON::XS->new->pretty(0)->encode ($_));
-   
-      
-   
-        $entry->{key}=$_;
-         $cmd->{$_} = JSON::XS->new->pretty(0)->encode ({"meta" => {"type" => $_,
-                                                                  "payload" => "overw"},
-                                                      "query" => $msg,
-                                                      "callback" => "makeCommon",
-                                                      "responce" => [$entry->{$_}]});
-
-            $self->send_frame(  $cmd->{$_} );
-      } 
-    }
-    
-    
-            
-     return $cmd;
-
-}
-
-
-
-
-sub printerFiles{
-
-
-  my ($self,$msg) = @_;
-
-     $msg = "space" unless($msg);
-     my $ret = {};
-     my $set = `perl $pwd/semantic.pl $msg`;
-     my @symbols = split("\n",$set);
-  
-
-
-  foreach my $k (@symbols[0..10]) {
-   $self->send_frame( encode_json({"meta" => {"type" => "overw",
-                         "payload" => $k},
-                              "query" => $k,
-                              "callback" => "makeCommon",
-                              "responce" => $k}) );
-  }
 }
 
 
@@ -122,14 +46,10 @@ my $server = Net::Async::WebSocket::Server->new(
          on_frame => sub {
             my ( $self, $frame ) = @_;
             $self->{frame} = $frame;
+            print STDOUT $frame;
             $list = printer($frame);
             $self->send_frame( $list );
-            $list = printerOverw($self, $frame);
-
-   #        
-            
-#            printerFiles($self,$frame);
-             #print Dumper $list;
+    
          },
       );
    }
@@ -142,6 +62,7 @@ $loop->add( $server );
 $server->listen(
    family => "inet",
    service => $PORT,
+   ip=>"127.0.0.1",
    on_listen => sub { print Dumper "Cannot listen - ",$_[-1];  },
    on_resolve => sub { print Dumper "Cannot resolve - ",$_[-1];},
    on_listen_error => sub { die "Cannot listen - $_[-1]" },
@@ -150,4 +71,6 @@ $server->listen(
 
 $loop->loop_forever;
 
+1;
 
+__END__
---
+++ bin/nav.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl -W
+use strict;
+use warnings;
+use JSON::XS;
+use Data::Dumper;
+use AI::MicroStructure;
+use Storable::CouchDB;
+my @ARGVX = ();
+
+
+our $json_main =  {lang=>"C",category=>"no",name=>"santex",size=>1,children=>[]};
+
+@ARGVX=("user",
+      "pass",
+      "localhost",
+      "nav") unless($#ARGVX>2);
+
+
+
+my $x = AI::MicroStructure->new;
+
+sub getAll {
+  my $key =shift;
+    
+  require LWP::UserAgent;
+  my $ua = LWP::UserAgent->new;
+  my ($server,$db) = (sprintf("http://%s:5984",$ARGVX[2]),"table");
+  my $res = $ua->get(sprintf('%s/%s/_design/base/_view/instances?reduce=false&start_key=["%s"]&end_key=["%sZZZ"]',
+                              $server,
+                              $db,
+                              $key,
+                              $key));
+
+
+my $content = decode_json($res->content);
+my @all;
+my @book;
+
+foreach(@{$content->{rows}}){
+
+  if(@{$_->{key}}){
+    push @all,grep{!/\W/}@{$_->{key}};
+  }
+}
+
+
+return @all;
+
+
+}
+
+
+
+
+$ARGV[0] = "" unless($ARGV[0]);
+my $key = sprintf("%s_%s",join("",@ARGVX),$ARGV[0]);
+my @datax = getAll($ARGV[0]);
+
+if($#datax){
+
+
+foreach my $i(0..$#datax) { 
+
+  printf("%s\n","".$datax[$i]) unless(!$datax[$i]);
+  
+}
+}
+
+1;
+__DATA__
+
+
